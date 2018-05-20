e851b7b03b475ad347293b8d9dd2ba3d291e6838
KamillaKhabibrakhmanova <venessiel@gmail.com>
2018-05-20 13:26:59 -0400

    add click event handler functionality

--- clickable-component.js
+++ clickable-component.js
@@ -3,6 +3,29 @@ AFRAME.registerComponent('log-on-click', {
     var el = this.el; // <a-entity>
     el.addEventListener('click', function() {
       console.log('clicked entity ' + el.id + '!');
+
+      builders.forEach(function(builder) {
+      	if (el.id === builder.name) {
+      		if (searchForBuilderTool(builder)){
+      			console.log('Thanks for finding my tool');
+      		} else {
+      			console.log(builder.dialogue);
+      		}
+      	}
+      });
+
+      tools.forEach(function(tool){
+      	if (el.id === tool.id) {
+      		console.log(tool.dialogue);
+      	}
+      });
+
     });
   }
 });
+
+function searchForBuilderTool(builder) {
+	userState.tools.some(function(tool) {
+		return tool.name === builder.tool.name;
+	});
+}
