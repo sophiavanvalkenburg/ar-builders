64e6485a7106a6c42a74dc4911cbc963357f1612
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2018-05-21 23:06:59 -0400

    click scene instead and look for whether marker is visible or not. also add tool

--- clickable-component.js
+++ clickable-component.js
@@ -1,30 +1,33 @@
-AFRAME.registerComponent('log-on-click', {
+AFRAME.registerComponent('accepts-clicks', {
   init: function() {
-    var el = this.el; // <a-entity>
-    el.addEventListener('click', function() {
-      console.log('clicked entity ' + el.id + '!');
-
-      builders.forEach(function(builder) {
-      	if (el.id === builder.name) {
-      		if (searchForBuilderTool(builder)){
-      			toggleSpeechBubble('Thanks for finding my tool');
-      		} else {
-      			toggleSpeechBubble(builder.dialogue);
-      		}
-      	}
-      });
-
-      tools.forEach(function(tool){
-      	if (el.id === tool.id) {
-      		toggleSpeechBubble(tool.dialogue);
-      	}
-      });
-
-    });
+    this.el.addEventListener('touchend', handleClickEvent);
+    this.el.addEventListener('click', handleClickEvent);
   }
 });
 
+function handleClickEvent() {
+  builders.forEach(function(builder) {
+    var builderMarker = document.querySelector("#" + builder.name + "-marker");
+    if (builderMarker && builderMarker.object3D.visible) {
+      if (searchForBuilderTool(builder)){
+        toggleSpeechBubble('Thanks for finding my tool');
+      } else {
+        toggleSpeechBubble(builder.dialogue);
+      }
+    }
+  });
+
+  tools.forEach(function(tool){
+    var toolMarker = document.querySelector("#" + tool.id + "-marker");
+    if (toolMarker && toolMarker.object3D.visible) {
+      toggleSpeechBubble(tool.dialogue);
+      if (!userState.hasBuilderTool(tool)) userState.addTool(tool);
+    }
+  });
+}
+
 function toggleSpeechBubble(dialogue) {
+  var speechBubble = document.querySelector(".speech-bubble");
 	if (speechBubble.style.display === 'none' || !speechBubble.style.display) {
 		speechBubble.innerHTML = dialogue;
 		speechBubble.style.display = 'block';
