2b910fdedc2435e32cc130936059d88a24797883
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2018-05-23 00:24:04 -0400

    hide speech bubble if no marker

--- js/interaction.js
+++ js/interaction.js
@@ -2,8 +2,29 @@ AFRAME.registerComponent('accepts-clicks', {
   init: function() {
     this.el.addEventListener('touchend', handleClickEvent);
     this.el.addEventListener('click', handleClickEvent);
+  },
+  tick: function() {
+    hideSpeechBubbleIfNoMarker();
   }
 });
+
+function hideSpeechBubbleIfNoMarker() {
+  var speechBubble = document.querySelector(".speech-bubble");
+  if (speechBubble.style.display === 'none' || !speechBubble.style.display) return;
+
+  var shouldHide = true;
+  builders.forEach(function(builder){
+    var builderMarker = document.querySelector("#" + builder.name + "-marker");
+    if (builderMarker & builderMarker.object3D.visible) shouldHide = false;
+  });
+
+  tools.forEach(function(tool){
+    var toolMarker = document.querySelector("#" + tool.name + "-marker");
+    if (toolMarker && toolMarker.object3D.visible) shouldHide = false;
+  });
+
+  if (shouldHide) speechBubble.style.display = 'none';
+};
 
 function handleClickEvent() {
   builders.forEach(function(builder) {
